4ffc6be527bfd821e37015d8291355699a1a4597
eneaslar <eneaslari@gmail.com>
2020-06-24 14:58:35 +0300

    new files

--- NodeJsServer/routes/index.js
+++ NodeJsServer/routes/index.js
@@ -8,7 +8,7 @@ router.get('/', function(req, res, next) {
 
 
 /* GET home page. */
-router.get('/fileload', function(req, res, next) {
+router.get('/stackedform', function(req, res, next) {
   // Website you wish to allow to connect
   res.setHeader('Access-Control-Allow-Origin', 'https://eneaslari.github.io')
   // Request methods you wish to allow
@@ -20,7 +20,22 @@ router.get('/fileload', function(req, res, next) {
   res.setHeader('Access-Control-Allow-Credentials', false);
   // Pass to next layer of middleware
   //res.send("TEXTTXTXT");
-  res.sendFile(path.join(__dirname, '../public', 'filetosend.txt'));
+  res.sendFile(path.join(__dirname, '../public', 'stackedform.html'));
+});
+
+router.get('/table', function(req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'https://eneaslari.github.io')
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', false);
+  // Pass to next layer of middleware
+  //res.send("TEXTTXTXT");
+  res.sendFile(path.join(__dirname, '../public', 'table.html'));
 });
 
 module.exports = router;
