09d6dac2a94f979eb443af9e0a41438c37866217
eneaslar <eneaslari@gmail.com>
2020-06-24 10:50:50 +0300

    make it better

--- NodeJsServer/routes/index.js
+++ NodeJsServer/routes/index.js
@@ -3,13 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express3333' });
 });
 
 
 /* GET home page. */
 router.get('/test', function(req, res, next) {
-  res.send('WE ARE OK MOTHERFUCKER!');
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'https://eneaslari.github.io/javascript_Learning/')
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', false);
+  // Pass to next layer of middleware
+  res.send('WE ARE OK MOTHERFUCKER! ASTA LA VISTA BABY');
 });
 
 module.exports = router;
